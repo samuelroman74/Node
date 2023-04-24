b8ed2010c572c5fd1b19d7574b0033ea1c598a7b
samuelroman74 <87458186+samuelroman74@users.noreply.github.com>
2023-04-23 20:07:01 -0400

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -4,6 +4,7 @@ const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 //Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
@@ -11,7 +12,7 @@ const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 //Exporter
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
+   const exporter = new JaegerExporter();
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
